3ec356886b911247c8940e79409cbe4038193fb8
Le Cong Dat <lcdatmin@gmail.com>
2019-04-08 22:23:32 +0700

    fix

---
+++ LibraryOnline/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using LibraryOnline.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LibraryOnline.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Admin()
+        {
+            return View();
+        }
+        // GET: Details
+        public ActionResult Details(int? id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+        
+    }
+}
